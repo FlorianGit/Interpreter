2e7e15775b4a9920b78be1a8ff71492bac3ffa95
FlorianGit <f.v.kluck@gmail.com>
2017-04-28 15:18:40 +0200

    Initial commit.
    A basic interpreter for arithmetic expressions. At this moment it supports
    multiplication and addition of integer values, with no spaces.

---
+++ src/main/scala/interpreter.scala
@@ -0,0 +1,34 @@
+object Interpreter {
+
+  abstract class Expr {
+    def evaluate(): Integer
+  }
+
+  case class Times(left:Expr, right: Expr) extends Expr {
+    def evaluate() = left.evaluate * right.evaluate
+  }
+
+  case class Plus(left: Expr, right: Expr) extends Expr {
+    def evaluate() = left.evaluate + right.evaluate
+  }
+
+  case class Number(x: Integer) extends Expr {
+    def evaluate() = x
+  }
+
+
+  def interpret(text: String): Expr = {
+    if (text contains '+') {
+      val splittedAtPlus = text split '+'
+      val tokens = splittedAtPlus map interpret
+      tokens reduce (Plus(_, _))
+    }
+    else if (text contains '*') {
+      val splittedAtTimes = text split '*'
+      val tokens = splittedAtTimes map interpret
+      tokens reduce (Times(_: Expr, _: Expr))
+    }
+    else Number(text.toInt)
+  }
+
+}
