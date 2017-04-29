b9ded3a64d180eedc3592a4566bd30835eea6f24
FlorianGit <f.v.kluck@gmail.com>
2017-04-29 18:49:35 +0200

    Add alternative interpreter

--- src/main/scala/interpreter.scala
+++ src/main/scala/interpreter.scala
@@ -1,9 +1,18 @@
 object Interpreter {
 
-  abstract class Expr {
+  trait Expr {
     def evaluate(): Integer
   }
 
+  type BinaryOperationTag = (Char, String)
+  val ops = Array(('+', "Plus"), ('-', "Minus"), ('*', "Times"))
+
+  def BinaryOperationFactory(str: String, left: Expr, right: Expr): Expr = str match {
+    case "Plus" => new Plus(left, right)
+    case "Minus" => new Minus(left,right)
+    case "Times" => new Times(left, right)
+  }
+    
   case class Times(left:Expr, right: Expr) extends Expr {
     def evaluate() = left.evaluate * right.evaluate
   }
@@ -39,4 +48,20 @@ object Interpreter {
     else Number(text.toInt)
   }
 
+  def interpret2(rawText:String) = {
+    def interpretHelper(rawText:String, ops: Array[BinaryOperationTag]): Expr = {
+      val text = rawText.trim
+      if (ops.isEmpty) Number(rawText.trim.toInt) else {
+        def op = ops.head
+        if (text contains op._1) {
+          val splitted = text split op._1
+          val tokens = splitted.map(interpretHelper(_, ops.tail))
+          tokens reduce ((x,y) => BinaryOperationFactory(op._2, x, y))
+        }
+        else interpretHelper(rawText, ops.tail)
+      }
+    }
+
+    interpretHelper(rawText, ops)
+  }
 }
--- src/test/scala/interpreterTest.scala
+++ src/test/scala/interpreterTest.scala
@@ -5,29 +5,35 @@ class FirstSpec extends FlatSpec {
    "Numbers" should "be interpreted correctly" in {
       assert(interpret("3") === Number(3))
       assert(interpret("345") === Number(345))
-      //assert(interpret("-35") === Number(-35)
+      assert(interpret2("3") === Number(3))
+      assert(interpret2("345") === Number(345))
+      /*assert(interpret("-35") === Number(-35))*/
    }
 
    "The char *" should "be interpreted to Times" in {
       assert(interpret("3*5") === Times(Number(3), Number(5)))
       assert(interpret("23*93") === Times(Number(23), Number(93)))
+      assert(interpret2("3*5") === Times(Number(3), Number(5)))
+      assert(interpret2("23*93") === Times(Number(23), Number(93)))
    }
 
    "Times" should "have higher priority than Plus" in {
-      assert(interpret("2+3*5") === Plus(Number(2), Times(Number(3),
-      Number(5))))
-      assert(interpret("1*3+6") === Plus(Times(Number(1), Number(3)),
-      Number(6)))
+      assert(interpret("2+3*5") === Plus(Number(2), Times(Number(3), Number(5))))
+      assert(interpret("1*3+6") === Plus(Times(Number(1), Number(3)), Number(6)))
+      assert(interpret2("2+3*5") === Plus(Number(2), Times(Number(3), Number(5))))
+      assert(interpret2("1*3+6") === Plus(Times(Number(1), Number(3)), Number(6)))
    }
 
    "Minus" should "be interpreted correctly" in {
       assert(interpret("5-3") === Minus(Number(5), Number(3)))
-      assert(interpret("5-6-4") === Minus(Minus(Number(5), Number(6)),
-      Number(4)))
+      assert(interpret("5-6-4") === Minus(Minus(Number(5), Number(6)), Number(4)))
+      assert(interpret2("5-3") === Minus(Number(5), Number(3)))
+      assert(interpret2("5-6-4") === Minus(Minus(Number(5), Number(6)), Number(4)))
    }
 
    "The string 3*5" should "interpret . evaluate to 15" in {
       val text = "3*5"
       assert(interpret(text).evaluate === 15)
+      assert(interpret2(text).evaluate === 15)
    }
 }
