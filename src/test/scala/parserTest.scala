6118649728fa64d4fc0fdfa80ab21825498e1bfe
FlorianGit <f.v.kluck@gmail.com>
2017-05-14 15:59:33 +0200

    Remove commented out test cases

--- src/test/scala/parserTest.scala
+++ src/test/scala/parserTest.scala
@@ -8,71 +8,41 @@ class parserSpec extends FlatSpec {
    "IntConsts" should "be interpreted correctly" in {
      var parser = new Parser(new Lexer("3"))
      assert(parser.expr() === Number(IntConst(3)))
-     //parser = new Parser(new Lexer("3"))
-     //var interpreter = new Interpreter()
-     //assert(interpreter.evaluate(parser.expr()) === 3)
 
      parser = new Parser(new Lexer("345"))
      assert(parser.expr() === Number(IntConst(345)))
-     //interpreter = new Interpreter()
-     //parser = new Parser(new Lexer("345"))
-     //assert(interpreter.evaluate(parser.expr()) === 345) 
    }
 
    "Plus" should "be interpreted correctly" in {
      var lexer = new Lexer("3+5+6")
      var parser = new Parser(lexer)
      assert(parser.expr() === BinOp(Plus(), BinOp(Plus(), Number(IntConst(3)), Number(IntConst(5))), Number(IntConst(6))))
-     //var interpreter = new Interpreter()
-     //lexer = new Lexer("3+5+6")
-     //parser = new Parser(lexer)
-     //assert(interpreter.evaluate(parser.expr()) === 14) 
    }
 
    "Minus" should "be interpreted correctly" in {
       var lexer = new Lexer("5-3")
       var parser = new Parser(lexer)
       assert(parser.expr() === BinOp(Minus(), Number(IntConst(5)), Number(IntConst(3))))
-     //var interpreter = new Interpreter()
-      //lexer = new Lexer("5-3")
-      //parser = new Parser(lexer)
-     //assert(interpreter.evaluate(parser.expr()) === 2) 
 
       lexer = new Lexer("5-6-4")
       parser = new Parser(lexer)
       assert(parser.expr() === BinOp(Minus(), BinOp(Minus(), Number(IntConst(5)), Number(IntConst(6))), Number(IntConst(4))))
-     //interpreter = new Interpreter()
-      //lexer = new Lexer("5-6-4")
-      //parser = new Parser(lexer)
-     //assert(interpreter.evaluate(parser.expr()) === -5) 
    }
 
    "Multiplication" should "work correctly" in {
      var lexer = new Lexer("3*5")
      var parser = new Parser(lexer)
      assert(parser.expr() === BinOp(Times(), Number(IntConst(3)), Number(IntConst(5))))
-     //var interpreter = new Interpreter()
-     //lexer = new Lexer("3*5")
-     //parser = new Parser(lexer)
-     //assert(interpreter.evaluate(parser.expr()) === 15) 
 
      lexer = new Lexer("3+6*5")
      parser = new Parser(lexer)
      assert(parser.expr() === BinOp(Plus(), Number(IntConst(3)), BinOp(Times(), Number(IntConst(6)), Number(IntConst(5)))))
-     //interpreter = new Interpreter()
-     //lexer = new Lexer("3+6*5")
-     //parser = new Parser(lexer)
-     //assert(interpreter.evaluate(parser.expr()) === 33) 
    }
 
    "Parentheses" should "go before all other operations" in {
      var lexer = new Lexer("(3+5)*2")
      var parser = new Parser(lexer)
      assert(parser.expr() === BinOp(Times(), BinOp(Plus(), Number(IntConst(3)), Number(IntConst(5))), Number(IntConst(2))))
-     //var interpreter = new Interpreter()
-     //lexer = new Lexer("(3+5)*2")
-     //parser = new Parser(lexer)
-     //assert(interpreter.evaluate(parser.expr()) === 16) 
 
      lexer = new Lexer("(3+5)*((2-1)+(4+3))")
      parser = new Parser(lexer)
@@ -81,47 +51,27 @@ class parserSpec extends FlatSpec {
      val fourplusthree = BinOp(Plus(), Number(IntConst(4)), Number(IntConst(3)))
      val rightside = BinOp(Plus(), twominusone, fourplusthree)
      assert(parser.expr() === BinOp(Times(), threeplusfive, rightside))
-     //interpreter = new Interpreter()
-     //lexer = new Lexer("(3+5)*((2-1)+(4+3))")
-     //parser = new Parser(lexer)
-     //assert(interpreter.evaluate(parser.expr()) === 64) 
 
      lexer = new Lexer("(12/(3+1))")
      parser = new Parser(lexer)
      val threeplusone = BinOp(Plus(), Number(IntConst(3)), Number(IntConst(1)))
      val total = BinOp(Div(), Number(IntConst(12)), threeplusone)
      assert(parser.expr() === total)
-     //interpreter = new Interpreter()
-     //lexer = new Lexer("(12/(3+1))")
-     //parser = new Parser(lexer)
-     //assert(interpreter.evaluate(parser.expr()) === 3) 
 
      lexer = new Lexer("12/(3+1)-1")
      parser = new Parser(lexer)
      val total2 =  BinOp(Minus(), total, Number(IntConst(1)))
      assert(parser.expr() === total2)
-     //interpreter = new Interpreter()
-     //lexer = new Lexer("12/(3+1)-1")
-     //parser = new Parser(lexer)
-     //assert(interpreter.evaluate(parser.expr()) === 2) 
 
      lexer = new Lexer("10/(12/(3+1)-1)")
      parser = new Parser(lexer)
      val total3 = BinOp(Div(), Number(IntConst(10)), total2)
      assert(parser.expr() === total3)
-     //interpreter = new Interpreter()
-     //lexer = new Lexer("10/(12/(3+1)-1)")
-     //parser = new Parser(lexer)
-     //assert(interpreter.evaluate(parser.expr()) === 5) 
 
      lexer = new Lexer("7+3*(10/(12/(3+1)-1))")
      parser = new Parser(lexer)
      val total4 = BinOp(Times(), Number(IntConst(3)), total3)
      assert(parser.expr() === BinOp(Plus(), Number(IntConst(7)), total4))
-     //interpreter = new Interpreter()
-     //lexer = new Lexer("7+3*(10/(12/(3+1)-1))")
-     //parser = new Parser(lexer)
-     //assert(interpreter.evaluate(parser.expr()) === 22) 
    }
 
    "Assigment" should "work" in {
